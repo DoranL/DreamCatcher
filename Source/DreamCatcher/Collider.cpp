e8b88871da0dbfaa94b2476a77670f5c65e86aa2
DoranL <ljhstarting414@gmail.com>
2022-09-03 22:52:08 +0900

    pawn movement
    w,a,s,d

---
+++ Source/DreamCatcher/Collider.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Collider.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ACollider::ACollider()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(GetRootComponent());
+	SphereComponent->InitSphereRadius(40.0f);   //충돌 범위
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));   //충돌 시 이름으로 구분 가능
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshComponentAsset(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+
+	if (MeshComponentAsset.Succeeded()) //생성자 도우미 F 객체 찾기 성공
+	{
+		MeshComponent->SetStaticMesh(MeshComponentAsset.Object); // 해당 개체는 구성 요소 자산과 일치하다?
+	}
+}
+
+
+// Called when the game starts or when spawned
+void ACollider::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollider::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/DreamCatcher/DreamCatcherGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DreamCatcherGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREAMCATCHER_API ADreamCatcherGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
