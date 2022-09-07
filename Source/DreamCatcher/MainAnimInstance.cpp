74cb227181c470ee91e8b27683b96849e9da4689
DoranL <ljhstarting414@gmail.com>
2022-09-07 22:49:32 +0900

    BasicCharacterMovement
    기본적인 이동

---
+++ Source/DreamCatcher/MainAnimInstance.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UMainAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		if (Pawn)
+		{
+			Pawn = TryGetPawnOwner(); //이 애니메이션 인스턴스의 소유자를 가져온다?
+		}
+	}
+}
+
+void UMainAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size();
+
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+
+		/*if (Main == nullptr)
+		{
+			Main = Cast<AMain>(Pawn);
+		}*/
+	}
+}
---
+++ Source/DreamCatcher/Nelia.cpp
@@ -0,0 +1,159 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Nelia.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+ANelia::ANelia()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create Camera boom(충격이 있는 경우 플레이어를 향해 당김)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 600.f; //camera follow at this distance
+	CameraBoom->bUsePawnControlRotation = true; //Rotate arm based on controller
+
+	//Set size for collision capsule
+	GetCapsuleComponent()->SetCapsuleSize(17.f, 64.f);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	//Attach the camera to the end of the boom and let the boom adjust to match
+	//the controller orientation
+	FollowCamera->bUsePawnControlRotation = false;
+
+	//Set our turn rates for input
+	BaseTurnRate = 65.f;
+	BaseLookUpRate = 65.f;
+
+	//Don't rotate when the controller rotates //카메라와 캐릭터가 동시 회전하는 걸 막는 역할?
+	//Let that just affect the camera
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+
+	//Configure character movement //입력 방향에 따라 자동으로 캐릭터가 그 방향을 보도록 회전하게 하는 건가?
+	GetCharacterMovement()->bOrientRotationToMovement = true; //character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 650.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	RunningSpeed = 650.f;
+	SprintingSpeed = 950.f;
+
+	bShiftKeyDown = false;
+}
+
+// Called when the game starts or when spawned
+void ANelia::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANelia::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ANelia::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ANelia::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ANelia::ShiftKeyDown);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ANelia::ShiftKeyUp);
+
+	//PlayerInputComponent->BindAction("LMB", IE_Pressed, this, &ANelia::LMBDown);
+	//PlayerInputComponent->BindAction("LMB", IE_Released, this, &ANelia::LMBUp);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ANelia::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ANelia::MoveRight);
+
+	PlayerInputComponent->BindAxis("turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &ANelia::TurnAtRate);
+	PlayerInputComponent->BindAxis("LokkUpRate", this, &ANelia::LookUpAtRate);
+}
+
+void ANelia::MoveForward(float Value)
+{
+	//bMovingForward = false;
+	if ((Controller != nullptr)) //&& (Value != 0.0f) && (!bAttacking) && (MovementStatus != EMovementStatus::EMS_Death))
+	{
+		//find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+
+		//bMovingForward = true;
+	}
+}
+
+void ANelia::MoveRight(float Value)
+{
+	//bMovingRight = false;
+	if ((Controller != nullptr) && (Value != 0.0f)) //&& (!bAttacking) && (MovementStatus != EMovementStatus::EMS_Death))
+	{
+		//find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+
+		//bMovingRight = true;
+	}
+}
+
+//키를 누르고 있으면 컨트롤러가 1초안에 65도 회전 가능   ///getworld?
+void ANelia::TurnAtRate(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ANelia::LookUpAtRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ANelia::SetMovementStatus(EMovementStatus Status)
+{
+	MovementStatus = Status;
+	if (MovementStatus == EMovementStatus::EMS_Sprinting)
+	{
+		GetCharacterMovement()->MaxWalkSpeed = SprintingSpeed;
+	}
+	else
+	{
+		GetCharacterMovement()->MaxWalkSpeed = RunningSpeed;
+	}
+}
+
+void ANelia::ShiftKeyDown()
+{
+	bShiftKeyDown = true;
+}
+
+void ANelia::ShiftKeyUp()
+{
+	bShiftKeyDown = false;
+}
